767673881b69bc933d277220600ed4585c7058d2
Webandclo <claudie.jousse@gmail.com>
2022-10-18 14:17:34 +0200

    ajout fichier Stuff et User

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,9 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 
 
-// Importation du router (fichier déporté)
+// Importation du routeur (fichier déporté)
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const router = express.Router();
 
@@ -19,10 +20,7 @@ const bodyParser = require('body-parser');
 // Donne accès au CORS de la requette
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     console.log('Je suis là ;-)');
-//     next();
-//  });
+
 app.use(helmet({
 crossOrigineResourcePolicy: false,
 }));
@@ -36,48 +34,6 @@ app.use((req, res, next) => {
 });
 
  
-// app.use((req, res, next) => {
-//    res.status(201);
-//    next();
-// });
-
-// Intercepté les requetes Post
-// Créer une routes pour votre application
-// Le front-end peut effectuer des appels vers l'application en toute sécurité.
-// app.post('/api/stuff', (req, res, next) => {
-//    console.log(req.body); // contenu du Corp de la requete
-//    res.status(201).json({ // Donner une réponse sinon la requete plante
-//      message: 'Objet créé !'
-//    });
-//  });
-
-
-// app.use((req, res) => {
-//    res.json({ message: 'Votre requête a bien été reçue !!!' }); 
-// });
-
-// app.get('/api/stuff', (req, res, next) => {
-//    const stuff = [
-//       {
-//       _id: '1234',
-//       title: 'Mon titre',
-//       description: 'Mes informations',
-//       imageUrl: 'http',
-//       price: 3900,
-//       userid: 'userid'
-//       },
-//       {
-//          _id: '12345',
-//          title: 'Mon titre',
-//          description: 'Mes informations',
-//          imageUrl: 'http',
-//          price: 4900,
-//          userid: 'userid'
-//          }
-//    ];
-//    res.status(200).json(stuff);
-// })
-
 
 
 // Connexion de l'API à MongoDB Atlas
@@ -91,54 +47,10 @@ mongoose.connect('mongodb+srv://claudie:OpenClassRooms@cluster0.bsal5sf.mongodb.
 
 app.use(bodyParser.json());
 
-// Logique importé et appliqué à la même route
-app.use('/api/stuff', stuffRoutes)
-
-
-
-//// EXERCICE QUIZ 2 ////
-
-
-// Implémentation de la route GET
-app.get('/api/products/', (req, res, next) => {
-  Product.find()  // Retourne tous les modèles Things 
-    .then(products => res.status(200).json(products)) // Renvoie un tableau contenant tous les Things
-    .catch(error => res.status(400).json({ error })); // Renvoie un tableau d'erreur
-});
-
-// Implémentation de la route POST
-app.get('/api/products/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id }) // findOne = Trouver un seule object
-                                        // Route dynamique, accessible en tant que paramètre 
-    .then(product => res.status(200).json(product))
-    .catch(error => res.status(404).json({ error })); // Renvoie un tableau d'erreur au front-end
-});
-
- // Création d'un objet existant avec le route POST
- app.post('/api/products/', (req, res, next) => {
-
-  // Création du formulaire de la sauce dans l'objet 'sauce'
-  const product = new Product({
-    ...req.body,
-  });
-
-  product.save()
-    .then((product) => res.status(200).json({ product }))
-    .catch(error => res.status(400).json({ error })); // Renvoie un tableau d'erreur au front-end
-});
-
- // Modification d'un objet existant avec le route PUT
- app.put('/api/products/:id', (req, res, next) => {
-  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error })); // Renvoie un tableau d'erreur au front-end
-});
-
-// Suppression d'un Thing avec le route delete
-app.delete('/api/products/:id', (req, res, next) => {
-  Product.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+// Logique importé et appliqué à la même route 
+// Routes attendu par le frontend
+// On enregistre les routeurs dans l'API
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
